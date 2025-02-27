f75fa2102578781443725dcc7051bb3049362df7
Kevin Francisco <kevinnico.francisco@amkor.com>
2025-02-27 14:37:44 +0800

    TESTING

--- db.php
+++ db.php
@@ -2,7 +2,9 @@
 
 $serverName = "10.199.9.61";
 $connectionOptions = [
-   
+    "Database" => "dev_playground",
+    "Uid" => "sscadm",
+    "PWD" => "s\$c@dm!098*"
 ];
 
 $conn = sqlsrv_connect($serverName, $connectionOptions);
